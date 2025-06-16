211e1955974de3ba14fa82afad069ba2f192b4c7
Dmitry <kalistratov.d.m@gmail.com>
2025-06-16 22:44:29 +0400

    add logs

--- server/src/main/kotlin/ru/kalistratov/famalot/Application.kt
+++ server/src/main/kotlin/ru/kalistratov/famalot/Application.kt
@@ -1,19 +1,31 @@
 package ru.kalistratov.famalot
 
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import org.slf4j.LoggerFactory
+
+private val logger =
+    LoggerFactory.getLogger("TEST")
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+    embeddedServer(
+        Netty,
+        port = SERVER_PORT,
+        host = "0.0.0.0",
+        module = Application::module
+    )
         .start(wait = true)
 }
 
 fun Application.module() {
+    logger.debug("START MODULE")
     routing {
         get("/") {
+            logger.warn("MAIN -KTOR")
             call.respondText("Ktor: ${Greeting().greet()}")
         }
     }
